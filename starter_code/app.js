d92874a4bf4cd51a1b9c46c3217968f2ed6d2399
Julio García Pérez <juliojose.garcia@11paths.com>
2018-01-10 22:10:08 +0100

    solution

---
+++ starter_code/app.js
@@ -0,0 +1,73 @@
+const SpotifyWebApi = require('spotify-web-api-node');
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+/* Spotify API Client config */
+
+const spotifyApi = new SpotifyWebApi({
+  clientId : '3531ef1cad0e43f5b041255aa9c38066',
+  clientSecret : '22ab1e554338461fac51a5a2d3756bd4'
+});
+
+spotifyApi.clientCredentialsGrant()
+  .then((data) => {
+    spotifyApi.setAccessToken(data.body.access_token);
+  }, (err) => {
+    console.log('Something went wrong when retrieving an access token', err);
+});
+
+/* Middlewares config */
+
+app.use(express.static('public'));
+app.use(expressLayouts);
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('layout', 'layout');
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+/* App routes */
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/artists', (req, res) => {
+  let artist = req.query.artist;
+
+  spotifyApi.searchArtists(req.query.artist)
+    .then(function(data) {
+      res.render('artists', {
+        artists: data.body.artists.items
+      });
+    }, function(err) {
+      console.log('Something went wrong!', err);
+    });
+});
+
+app.get('/albums/:artistId', (req, res, next) => {
+  spotifyApi.getArtistAlbums(req.params.artistId)
+    .then(function(data) {
+      res.render('albums', {
+        albums: data.body.items
+      });
+    }, function(err) {
+      console.log('Something went wrong!', err);
+    });
+});
+
+app.get('/tracks/:albumId', (req, res, next) => {
+  spotifyApi.getAlbumTracks(req.params.albumId)
+    .then(function(data) {
+      res.render('tracks', {
+        tracks: data.body.items
+      });
+    }, function(err) {
+      console.log('Something went wrong!', err);
+    });
+});
+
+app.listen(3000);
