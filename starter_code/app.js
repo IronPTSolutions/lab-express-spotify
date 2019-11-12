cfdb3e6a8281d7d0ebe95d9c5e0ba8d3d0395f95
Carol Alonso <c.alonsolp@gmail.com>
2019-11-12 11:55:01 +0100

    fix: change ejs to hbs

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,6 +1,5 @@
 const SpotifyWebApi = require('spotify-web-api-node');
 const express = require('express');
-const expressLayouts = require('express-ejs-layouts');
 const bodyParser = require('body-parser');
 
 const app = express();
@@ -8,8 +7,8 @@ const app = express();
 /* Spotify API Client config */
 
 const spotifyApi = new SpotifyWebApi({
-  clientId : '3531ef1cad0e43f5b041255aa9c38066',
-  clientSecret : '22ab1e554338461fac51a5a2d3756bd4'
+  clientId: '3531ef1cad0e43f5b041255aa9c38066',
+  clientSecret: '22ab1e554338461fac51a5a2d3756bd4'
 });
 
 spotifyApi.clientCredentialsGrant()
@@ -17,17 +16,15 @@ spotifyApi.clientCredentialsGrant()
     spotifyApi.setAccessToken(data.body.access_token);
   }, (err) => {
     console.log('Something went wrong when retrieving an access token', err);
-});
+  });
 
 /* Middlewares config */
 
 app.use(express.static('public'));
-app.use(expressLayouts);
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.set('layout', 'layout');
 app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.set('view engine', 'hbs');
 
 /* App routes */
 
@@ -39,33 +36,33 @@ app.get('/artists', (req, res) => {
   let artist = req.query.artist;
 
   spotifyApi.searchArtists(req.query.artist)
-    .then(function(data) {
+    .then(function (data) {
       res.render('artists', {
         artists: data.body.artists.items
       });
-    }, function(err) {
+    }, function (err) {
       console.log('Something went wrong!', err);
     });
 });
 
 app.get('/albums/:artistId', (req, res, next) => {
   spotifyApi.getArtistAlbums(req.params.artistId)
-    .then(function(data) {
+    .then(function (data) {
       res.render('albums', {
         albums: data.body.items
       });
-    }, function(err) {
+    }, function (err) {
       console.log('Something went wrong!', err);
     });
 });
 
 app.get('/tracks/:albumId', (req, res, next) => {
   spotifyApi.getAlbumTracks(req.params.albumId)
-    .then(function(data) {
+    .then(function (data) {
       res.render('tracks', {
         tracks: data.body.items
       });
-    }, function(err) {
+    }, function (err) {
       console.log('Something went wrong!', err);
     });
 });
